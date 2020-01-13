d6fba32101f4f9b4ad1d71ec77868597fc9f5ee2
Sanchez <skrolsky@gmail.com>
2020-01-13 13:31:29 +0300

    Update envs.sh

--- envs.sh
+++ envs.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+export KEY_JSON=/home/keys/key-packer.json
+export ZONE=us-east1-b
+export MACHINE_TYPE=n1-standard-1
+export IMAGE_DESCR=packer-image
+# USER in GCP created automatic
+
+export PROJECT=$(gcloud info --format='value(config.project)')
 export IMAGE_PACKER=$(gcloud compute images list --format="value(NAME)" --filter=ubuntu-1804)
