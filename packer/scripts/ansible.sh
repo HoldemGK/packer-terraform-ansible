eda517a1b5a329e8f97aabccf7e1a05c61e750ef
Sanchez <skrolsky@gmail.com>
2020-01-20 10:52:52 +0300

    update sh scripts

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Install Ansible repository.
-apt -y update && apt-get -y upgrade
-apt -y install software-properties-common
-apt-add-repository ppa:ansible/ansible
+sudo apt-get -y update
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
 
 # Install Ansible.
-apt -y update
-apt -y install ansible
+sudo apt -y update
+sudo apt -y install ansible
