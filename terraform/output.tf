88036bf5cd5ebf640f29d246e3f5a0949b2f4efb
Sanchez <skrolsky@gmail.com>
2020-02-18 20:37:02 +0300

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,9 +2,9 @@ output "ip" {
   value = "${google_compute_instance.staging.network_interface.0.access_config.0.nat_ip}"
 }
 
-output "boot_image" {
-  value = google_compute_instance.staging.boot_disk[initialize_params].image
-}
+#output "boot_image" {
+#  value = google_compute_instance.staging.boot_disk[initialize_params].image
+#}
 
 output "instance_id" {
   value = google_compute_instance.staging.self_link
